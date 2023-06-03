5b2f0234a943f2f738343207f7870cd43d132f11
Akshata Ganbote <117456092+AkshataGanbote@users.noreply.github.com>
2023-06-03 14:04:16 +0530

    2

---
+++ 2_Compiling TS/index.js
@@ -0,0 +1,7 @@
+var character = 'mario';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
