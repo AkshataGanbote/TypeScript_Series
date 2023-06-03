963fbb6ab384c4893f19c91f2e727e750b7ffb9e
Akshata Ganbote <117456092+AkshataGanbote@users.noreply.github.com>
2023-06-03 14:03:41 +0530

    TypeScript Series

--- 2_Compiling TS/index.js
+++
@@ -1,7 +0,0 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
--- 2_Compiling TS/index.ts
+++
@@ -1,9 +0,0 @@
-const character = 'mario';
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-
-inputs.forEach(input =>{
-    console.log(input)
-});
