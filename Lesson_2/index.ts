8e3bf8027599bed9fb62a14bcce187cf95a1e9c5
Akshata Ganbote <117456092+AkshataGanbote@users.noreply.github.com>
2023-06-03 12:51:16 +0530

    Lesson 2

---
+++ Lesson_2/index.ts
@@ -0,0 +1,9 @@
+const character = 'mario';
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input =>{
+    console.log(input)
+});
