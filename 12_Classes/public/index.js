d0a3e8262f5fccc273e7e21743fb8cb921d70229
Akshata Ganbote <117456092+AkshataGanbote@users.noreply.github.com>
2023-06-14 22:43:41 +0530

    12

---
+++ 12_Classes/public/index.js
@@ -0,0 +1,28 @@
+"use strict";
+//(A) Exapmle 1: Passing tag under querySelector
+const anchor = document.querySelector('a');
+console.log(anchor);
+// console.log(anchor.href); //Error
+//Insteadd We can do following 2 things 
+//1 :- Adding If statement  
+if (anchor) {
+    console.log(anchor.href);
+}
+// OR
+// 2:- Adding ! after querySelector 
+const anchor1 = document.querySelector('a'); //put ! after bracket
+console.log(anchor1.href);
+// (B) Exapmle 2: Passing any class or id under querySelector 
+const form = document.querySelector('.new-item-form');
+console.log(form);
+//inputs 
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// console.log(type) -access 1 element only
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber //We want to view value as number(valueAsDate) than String
+    );
+});
